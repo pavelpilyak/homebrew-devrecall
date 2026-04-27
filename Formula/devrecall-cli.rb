b170826f2f5a97e977579d290a1bd0420c5f9c45
Pavel <pavelpilyak@gmail.com>
2026-04-27 21:27:38 +0200

    devrecall 0.1.0 (manual SHA bump)

--- Formula/devrecall-cli.rb
+++ Formula/devrecall-cli.rb
@@ -7,22 +7,22 @@ class DevrecallCli < Formula
   on_macos do
     on_arm do
       url "https://github.com/pavelpilyak/devrecall/releases/download/v#{version}/devrecall-darwin-aarch64.tar.gz"
-      sha256 "PLACEHOLDER_DARWIN_AARCH64_SHA256"
+      sha256 "a4a499da580674472f3e6b54543246f6298316d55b3c237e562e24b8829a664f"
     end
     on_intel do
       url "https://github.com/pavelpilyak/devrecall/releases/download/v#{version}/devrecall-darwin-x86_64.tar.gz"
-      sha256 "PLACEHOLDER_DARWIN_X86_64_SHA256"
+      sha256 "76269435404c6581bae173685a0bf97a25842acb986874d3925e721887c9ccad"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/pavelpilyak/devrecall/releases/download/v#{version}/devrecall-linux-aarch64.tar.gz"
-      sha256 "PLACEHOLDER_LINUX_AARCH64_SHA256"
+      sha256 "10ce89b24a96e15c3760746d1c4a4e7d991bb0a82f9b5f4a643a28fbf586ad49"
     end
     on_intel do
       url "https://github.com/pavelpilyak/devrecall/releases/download/v#{version}/devrecall-linux-x86_64.tar.gz"
-      sha256 "PLACEHOLDER_LINUX_X86_64_SHA256"
+      sha256 "55d69b3a1f032273e63c7a5149062c6897f62de010f31d283387e9a47b922667"
     end
   end
 
